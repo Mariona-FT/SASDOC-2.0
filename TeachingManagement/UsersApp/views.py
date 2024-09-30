0a5c88209e074ee51cd4e247287842ce26631734
Mariona Farré Tapias <marionafarretapias@gmail.com>
2024-09-30 18:07:27 +0200

    user has a new caracteristic role
    role: director,professor,sector-cheif,

--- TeachingManagement/UsersApp/views.py
+++ TeachingManagement/UsersApp/views.py
@@ -3,17 +3,29 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def is_director(user):
+    return user.role == 'director'
+
 @login_required
+#@user_passes_test(is_director)
 def director_dashboard(request):
     pass
     #return render(request, 'users/director_dashboard.html')
 
+def is_sector_chief(user):
+    return user.role == 'sector_chief'
+
 @login_required
+#@user_passes_test(is_sector_chief)
 def sector_chief_dashboard(request):
     pass
     #return render(request, 'users/sector_chief_dashboard.html')
 
+def is_professor(user):
+    return user.role == 'professor'
+
 @login_required
+#@user_passes_test(is_professor)
 def professor_dashboard(request):
     pass
     #return render(request, 'users/professor_dashboard.html')
