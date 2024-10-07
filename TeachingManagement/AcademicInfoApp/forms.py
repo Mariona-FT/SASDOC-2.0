bc772df32301387a08cefbc296fd99a4ed80b931
Mariona Farré Tapias <marionafarretapias@gmail.com>
2024-10-07 15:17:00 +0200

    form schools with crud implemented

--- TeachingManagement/AcademicInfoApp/forms.py
+++ TeachingManagement/AcademicInfoApp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Field,Section,School
+from .models import Field,Section,School,Degree
 
 class FieldForm(forms.ModelForm):
     class Meta:
@@ -15,3 +15,8 @@ class SchoolForm(forms.ModelForm):
     class Meta:
         model = School
         fields = ['NameSchool', 'Section','isActive']
+
+class DegreeForm(forms.ModelForm):
+    class Meta:
+        model = Degree
+        fields = ['NameDegree', 'School','isActive']
