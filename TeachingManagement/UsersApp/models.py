1c24bcde6515a9bba08587bc525bd7cfad1812d2
Mariona Farré Tapias <marionafarretapias@gmail.com>
2024-10-23 11:47:10 +0200

    Better Register for Sectorchiefs+admin field+lenguages

--- TeachingManagement/UsersApp/models.py
+++ TeachingManagement/UsersApp/models.py
@@ -66,9 +66,10 @@ class ProfessorField(models.Model):
     idProfessorField = models.AutoField(primary_key=True)
     Professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='professor_fields')
     Field = models.ForeignKey(Field, on_delete=models.CASCADE)
+    Comment=models.TextField(null=True,blank=True)
 
     def __str__(self):
-        return f"{self.Professor.name} - {self.field.NameField}"
+        return f"{self.Professor.name} - {self.Field.NameField}"
 
 # PROFESSOR-LANGUAGE MODEL (Many-to-Many)
 class ProfessorLanguage(models.Model):
