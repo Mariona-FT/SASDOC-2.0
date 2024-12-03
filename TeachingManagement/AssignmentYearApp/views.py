2c4e39f38e246a4b2286a7fea84d5132b00ac577
Mariona Farré Tapias <marionafarretapias@gmail.com>
2024-12-03 12:09:51 +0100

    Listing of CourseYears now with the correct info for the different points

--- TeachingManagement/AssignmentYearApp/views.py
+++ TeachingManagement/AssignmentYearApp/views.py
@@ -73,39 +73,56 @@ def section_courses_list(request):
             point_name = getattr(typepoints_section, field)
             if point_name:  
                 typepoint_names.append(point_name)
-
+    
+    typepoint_names_assigned={}
+    #Dicc to have the names linked for the possible points
+    typepoints_fields = {
+        'PointsA': typepoints_section.NamePointsA,
+        'PointsB': typepoints_section.NamePointsB,
+        'PointsC': typepoints_section.NamePointsC,
+        'PointsD': typepoints_section.NamePointsD,
+        'PointsE': typepoints_section.NamePointsE,
+        'PointsF': typepoints_section.NamePointsF
+    }
+    #Only save the names of the groups that are not None
+    for field, point_name in typepoints_fields.items():
+        if point_name is not None:
+            typepoint_names_assigned[field] = point_name
+    
     course_data = [] #all info of ONE COURSE
    
     for course_year in course_years:
-        points = {}
+        total_points = {}
         total_course_points = 0         #total points TO assign in the course - initial points
         total_assigned_points = 0       #total ALREADY assigned points - where professors wil be assigned
-
-        #Return info of TOTAL POINTS - that will need to be assigned that Course year
-        for i, point_name in enumerate(typepoint_names):
-            point_field = f'Points{chr(65 + i)}'  # PointsA, PointsB..
-            point_value = getattr(course_year, point_field, None)
-            points[point_name] = point_value if point_value is not None else 0  
-            
-            total_course_points += points[point_name]  
+        
+        # Return info of TOTAL POINTS - that will need to be assigned that Course year
+        total_points = {}
+        for field, point_name in typepoint_names_assigned.items():
+            point_value = getattr(course_year,field, 0) or 0
+            total_points[point_name] = point_value
+        
+            total_course_points = sum(value if value is not None else 0 for value in total_points.values())
         
         #Return info of INFO POINTS ALREADY ASSIGNED x professor that Course year
-        assigned_points = {point_name: 0 for point_name in typepoint_names}
-       
-        assignments = Assignment.objects.filter(CourseYear=course_year)  
+        #return ASSIGNEDPOINTSS from assignment
+        assigned_points = {name: 0 for name in typepoint_names}
+        assignments = Assignment.objects.filter(CourseYear=course_year)
+
         for assignment in assignments:
-            for i, point_name in enumerate(typepoint_names):
-                point_field = f'Points{chr(65 + i)}'  # PointsA, PointsB..
-                assigned_point_value = getattr(assignment, point_field, None) or 0
-                if assigned_point_value is not None:
-                    assigned_points[point_name] += assigned_point_value
-                    total_assigned_points += assigned_point_value 
+            for field, point_name in typepoint_names_assigned.items():
+                assigned_value = getattr(assignment, field, 0) or 0
+                if assigned_value is not None:
+                    assigned_points[point_name] += assigned_value
+
+        total_assigned_points = sum(value if value is not None else 0 for value in assigned_points.values())
         
         # Create formatted string for each point type in "Group: assigned/total" format
         points_summary = ""
-        for point_name in typepoint_names:
+        # for point_name in typepoint_names:
+        for field, point_name in typepoint_names_assigned.items():
             assigned_value = assigned_points.get(point_name, 0)
-            total_value = points.get(point_name, 0)
+            total_value = total_points.get(point_name, 0)
             points_summary += f"{point_name}: {assigned_value}/{total_value} , "
 
         # Remove the trailing comma and space
@@ -141,8 +158,6 @@ def courseyear_show(request,idCourseYear=None):
         Year=course_year.Year
     ).first()
 
-    print(f"section: {section}")
-
     #extract the NAMES OF TYPE POINTS of that section - only save the not empty names 
     typepoint_names = []
     if typepoints_section:
@@ -152,61 +167,7 @@ def courseyear_show(request,idCourseYear=None):
             if point_name:  
                 typepoint_names.append(point_name)
 
-    
-    print(f"typepoint_names: {typepoint_names}")
-
-    #return TOTALPOINTS from CourseYear
-    total_points = {}
-    for i, point_name in enumerate(typepoint_names):
-        point_field = f'Points{chr(65 + i)}'  # PointsA, PointsB, etc.
-        point_value = getattr(course_year, point_field, 0) or 0
-        total_points[point_name] = point_value
-    
-        total_points_sum = sum(value if value is not None else 0 for value in total_points.values())
-
-    print(f"total_points: {total_points}")
-
-    #return ASSIGNEDPOINTSS from assignment
-    assigned_points = {name: 0 for name in typepoint_names}
-
-    assignments = Assignment.objects.filter(CourseYear=course_year)
-    for assignment in assignments:
-        for i, point_name in enumerate(typepoint_names):
-            point_field = f'Points{chr(65 + i)}'  # PointsA, PointsB, etc.
-            assigned_value = getattr(assignment, point_field, 0) or 0
-            if assigned_value is not None:
-                assigned_points[point_name] += assigned_value
-
-    assigned_points_sum = sum(value if value is not None else 0 for value in assigned_points.values())
-
-    print(f"assigned_points (after assignments): {assigned_points}")
-
-    #return PROFESSORS already ASSIGNED in that COURSEYEAR
-    professors = Professor.objects.filter(assignment__CourseYear=course_year).distinct()
-
-    # Calculate the assigned points for each professor
-    professor_assigned_points = {}
-    for professor in professors:
-        professor_points = {}
-        for i, point_name in enumerate(typepoint_names):
-            # Get the assigned points for this professor in this typepoint
-            total_assigned_points = 0
-            assignments = Assignment.objects.filter(
-                Professor=professor,
-                CourseYear=course_year
-            )
-            for assignment in assignments:
-                point_field = f'Points{chr(65 + i)}'
-                total_assigned_points += getattr(assignment, point_field, 0) or 0
-            professor_points[point_name] = total_assigned_points
-        
-        #list of al professors with their points for each typepoint in that section
-        professor_assigned_points[professor] = professor_points 
-
-    print(f"PROFESSORS: {professor_assigned_points}")
-    
     typepoint_names_assigned={}
-
     #Dicc to have the names linked for the possible points
     typepoints_fields = {
         'PointsA': typepoints_section.NamePointsA,
@@ -216,50 +177,63 @@ def courseyear_show(request,idCourseYear=None):
         'PointsE': typepoints_section.NamePointsE,
         'PointsF': typepoints_section.NamePointsF
     }
-
     #Only save the names of the groups that are not None
     for field, point_name in typepoints_fields.items():
         if point_name is not None:
             typepoint_names_assigned[field] = point_name
+    
+    #return TOTALPOINTS from CourseYear
+    total_points = {}
+    for field, point_name in typepoint_names_assigned.items():
+        point_value = getattr(course_year,field, 0) or 0
+        total_points[point_name] = point_value
+    
+        total_points_sum = sum(value if value is not None else 0 for value in total_points.values())
 
-    print(f"Names Assigned: {typepoint_names_assigned}")
+    #return ASSIGNEDPOINTSS from assignment
+    assigned_points = {name: 0 for name in typepoint_names}
+    assignments = Assignment.objects.filter(CourseYear=course_year)
+
+    for assignment in assignments:
+        for field, point_name in typepoint_names_assigned.items():
+            assigned_value = getattr(assignment, field, 0) or 0
+            if assigned_value is not None:
+                assigned_points[point_name] += assigned_value
+
+    assigned_points_sum = sum(value if value is not None else 0 for value in assigned_points.values())
+
 
     #return ONLY the ASSIGNED PROFESSORS - it will be in the Assignment table in that CourseYear
     assignments = Assignment.objects.filter(CourseYear=course_year)
-    print(f"Assignments: {assignments}")
 
     #Save the info of the PROFESSORS already assgned
     assignment_data = []
     for assignment in assignments:
         data = {}
         data['id']=assignment.idAssignment
-        data['professor_name'] = professor.name +" "+professor.family_name
-        data['professor_id'] = professor.idProfessor  # Store the professor's ID
         data['is_coordinator']=assignment.IsCoordinator
+
+        data['professor_name'] = assignment.Professor.name +" "+assignment.Professor.family_name
+        data['professor_id'] = assignment.Professor.idProfessor
+        
         for field, point_name in typepoint_names_assigned.items():
             value = getattr(assignment, field, None) or 0
             data[point_name] = value
-            print(f"Field: {field}, Value: {data[point_name]}")  
 
         assignment_data.append(data)
 
-    print(f"Assigned Data: {assignment_data}")
-
     context = {
         'course_year': course_year,
-        'typepoint_names': typepoint_names,
-
-        'total_points': total_points,
-        'assigned_points': assigned_points,
-
-        'total_points_sum':total_points_sum,
-        'assigned_points_sum': assigned_points_sum,
-
-        'professors': professors,
-        'professor_assigned_points': professor_assigned_points,
-        
+        #list of the type of points-the name given
         'typepoint_names_assigned': typepoint_names_assigned,
 
+        #Table of actual total points and assigned points
+        'total_points': total_points,
+        'assigned_points': assigned_points,
+        'total_points_sum':total_points_sum,
+        'assigned_points_sum': assigned_points_sum,
+        
+        #Table professors assigned
         'assignment_data': assignment_data,
 
     }
