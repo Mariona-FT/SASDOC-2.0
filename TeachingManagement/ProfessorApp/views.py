577e9aa04e589db888cc741b9df6e92b0af19ea7
Mariona Farré Tapias <marionafarretapias@gmail.com>
2025-01-13 18:45:06 +0100

    finalizing pdf report for professor
    little changes - report for the selected year, not the last one

--- TeachingManagement/ProfessorApp/views.py
+++ TeachingManagement/ProfessorApp/views.py
@@ -26,115 +26,6 @@ def is_professor(user):
 def professor_dashboard(request):
     return render(request, 'professor_dashboard.html')
 
-# @login_required
-# @user_passes_test(is_professor)
-# def info_assignments(request):
-#     available_years = Year.objects.all().order_by('-Year').distinct()
-    
-#     # Selected year ID and object
-#     selected_year_id = request.GET.get('year')
-#     selected_year = None
-
-#     # Try to retrieve the selected year, default to the most recent year
-#     try:
-#         selected_year = Year.objects.get(pk=selected_year_id)
-#         selected_year_id = int(selected_year_id) if selected_year_id else 0
-        
-#     except (ValueError, Year.DoesNotExist):
-#         selected_year = Year.objects.order_by('-Year').first()
-#         selected_year_id = selected_year.idYear if selected_year else None
-    
-#     if not selected_year:
-#         messages.error(request, "No hi ha cursos acadèmics disponibles.")
-#         return render(request, 'info_assignments_professor.html', {'available_years': available_years})
-    
-   
-#     user = request.user
-#     professor=None
-#     try:
-#         professor = Professor.objects.get(user=user)
-#     except Professor.DoesNotExist:
-#         messages.error(request, "No es troba el professor per l'usuari actual.")
-#         return render(request, 'info_assignments_professor.html', {'available_years': available_years})
-
-#     professor_data=[]
-#     # Filter capacity, free, and capacity_section entries by the selected year
-#     capacities = Capacity.objects.filter(Professor=professor, Year=selected_year).order_by('-Year__Year')
-#     frees = Free.objects.filter(Professor=professor, Year=selected_year).order_by('-Year__Year')
-#     capacity_sections = CapacitySection.objects.filter(Professor=professor, Year=selected_year).order_by('-Year__Year')
-   
-
-#     professor_data.append({
-#         'capacities': capacities,
-#         'frees': frees,
-#         'capacity_sections': capacity_sections,
-#     })
-
-
-#     #Get ALL COURSES for each SECTION assigned
-#     assignments = Assignment.objects.filter(
-#         Professor=professor, 
-#         CourseYear__Year__idYear=selected_year_id
-#     ).select_related('CourseYear', 'CourseYear__Course', 'CourseYear__Year', 
-#                     'CourseYear__Course__Degree', 'CourseYear__Course__Degree__School', 
-#                     'CourseYear__Course__Degree__School__Section')
-
-#     # Prepare sections data with the corresponding points for each section
-#     sections_info = {}
-#     for capacity_section in capacity_sections:
-#         section_name = capacity_section.Section.NameSection
-#         if section_name not in sections_info:
-#             sections_info[section_name] = []
-
-#     course_years = {assignment.CourseYear for assignment in assignments}
-#     for course_year in course_years:
-#         # Filter assignments for this specific CourseYear
-#         section_assignments = [a for a in assignments if a.CourseYear == course_year]
-
-#         # Calculate total points for the section
-#         total_points = sum(
-#             (a.PointsA or 0) + (a.PointsB or 0) + (a.PointsC or 0) +
-#             (a.PointsD or 0) + (a.PointsE or 0) + (a.PointsF or 0)
-#             for a in section_assignments
-#         )
-
-#         # Get coordinator and coworkers
-#         coordinator = next((a.Professor for a in section_assignments if a.isCoordinator), "")
-#         coworkers = [
-#             a.Professor for a in Assignment.objects.filter(CourseYear=course_year)
-#         ]
-
-#         # Append data to result dictionary            
-#         section_name = course_year.Course.Degree.School.Section.NameSection
-#         if section_name not in sections_info:
-#             sections_info[section_name] = []
-
-#         points_summary = get_assigned_points_summary(course_year, professor)
-
-#         sections_info[section_name].append({
-#             'school': course_year.Course.Degree.School.NameSchool,
-#             'degree': course_year.Course.Degree.NameDegree,
-#             'course': course_year.Course.NameCourse,
-#             'semester': course_year.Semester,
-#             'total_points': total_points,
-#             'points_summary':points_summary,
-#             'coordinator': coordinator,
-#             'coworkers': coworkers,
-#         })
-
-        
-#     context = {
-#         'professor':professor,
-#         'available_years': available_years,
-#         'selected_year': selected_year,
-
-#         'professor_data': professor_data,        
-#         'sections_info': sections_info,
-
-#     }
-
-#     return render(request, 'info_assignments_professor.html',context)
-
 def get_professor_assignments_data(request):
     available_years = Year.objects.all().order_by('-Year').distinct()
     selected_year_id = request.GET.get('year')
@@ -265,6 +156,19 @@ def info_assignments(request):
 @login_required
 @user_passes_test(is_professor)
 def generate_assignments_pdf(request):
+    
+    selected_year_id = request.GET.get('year')
+    selected_year = None
+    if selected_year_id:
+        try:
+            selected_year = Year.objects.get(pk=selected_year_id)
+        except Year.DoesNotExist:
+            selected_year = None
+    
+    if not selected_year:
+        # If no year is selected, use the most recent year
+        selected_year = Year.objects.order_by('-Year').first()
+
     context, error = get_professor_assignments_data(request)
     if error:
         messages.error(request, error['error'])
@@ -272,17 +176,27 @@ def generate_assignments_pdf(request):
 
     render(request, 'info_assignments_professor.html', context)
 
+    user = request.user
+    try:
+        professor = Professor.objects.get(user=user)
+    except Professor.DoesNotExist:
+        return None, {"error": "No es troba el professor per l'usuari actual."}
+
+
+    professor_name = f"{professor.name}_{professor.family_name}".replace(" ", "_")
+    year_label = selected_year.Year if selected_year else "sense_any"
+
+    filename = f"assignacio_docent_{professor_name}_{year_label}.pdf"
+   
     # Generate HTML from template
     html = render_to_string('pdf_template.html', context)
 
-    # Create an HttpResponse object for PDF output
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="assignments.pdf"'
+    response['Content-Disposition'] = f'attachment; filename="{filename}"'
 
     # Convert HTML to PDF
     pisa_status = pisa.CreatePDF(html, dest=response)
 
-    # If an error occurs, display the HTML for debugging
     if pisa_status.err:
         return HttpResponse('Errors occurred while generating PDF:<pre>' + html + '</pre>')
 
